573eec1c780326b0c882fd220f0a9b1d584c0c29
Sergio Cruz <vlad.crz@gmail.com>
2023-05-21 23:18:24 -0600

    Initial project commit

---
+++ src/main/java/com/infomedia/tokenbasedauthenticationrestapi/TokenBasedAuthenticationRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.infomedia.tokenbasedauthenticationrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenBasedAuthenticationRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TokenBasedAuthenticationRestApiApplication.class, args);
+    }
+
+}
